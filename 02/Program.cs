dee3d1f5a8311c374c852dc1c17f66b7bd3f9428
Breno Gomes <78120805+gomesbrnn@users.noreply.github.com>
2022-03-31 17:06:21 -0300

    02

---
+++ 02/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seja bem vindo! Tudo bem?" + "\n");
+            Console.WriteLine("Poderia me informar em que turno você trabalha?" + "\n");
+            Console.WriteLine("Para informar seu turno digite uma das opções: " + "\n");
+            Console.WriteLine("M-matutino");
+            Console.WriteLine("V-vespertino");
+            Console.WriteLine("N-noturno");
+            Console.WriteLine();
+
+            string Turno = (Console.ReadLine());
+
+            if(Turno == "M")
+            {
+                Console.WriteLine("Bom dia!");
+            }
+            else if(Turno == "V")
+            {
+                Console.WriteLine("Boa tarde!");
+            }
+            else if(Turno == "N")
+            {
+                Console.WriteLine("Boa noite!");
+            }
+            else
+            {
+                Console.WriteLine("Valor invalido");
+            }
+        }
+    }
+}
